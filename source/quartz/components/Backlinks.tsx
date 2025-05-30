ebb77d1c46ee3134d87634420d257e855feb9639
DCSG <zomblovr@hotmail.com>
2025-05-30 13:59:41 -0700

    vault backup: 2025-05-30 13:59:41

--- source/quartz/components/Backlinks.tsx
+++ source/quartz/components/Backlinks.tsx
@@ -18,7 +18,7 @@ const Backlinks: QuartzComponent = ({
       <ul class="overflow">
         {backlinkFiles.length > 0 ? (
 		  (backlinkFiles.sort(
-			(a, b) => a.displayName.localeCompare(b.displayName, undefined, {numeric: true, sensitivity: "base",})
+			(a, b) => a.displayName.localeCompare(b.displayName, "en", {numeric: true, sensitivity: "base",})
 		  )).map((f) => (
             <li>
               <a href={resolveRelative(fileData.slug!, f.slug!)} class="internal">
