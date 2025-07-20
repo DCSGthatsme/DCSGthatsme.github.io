4f5574c553a8c28dd7a3fe0c133adb4a1b070e6d
DCSG <zomblovr@hotmail.com>
2025-07-20 15:00:44 -0700

    Revert "."
    This reverts commit 1e89c642c637299d137a33f2d680f65ac974e4b0.

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -5,21 +5,24 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [Component.Breadcrumbs(),],
-  footer: Component.Footer(),
+  footer: Component.Footer({
+    links: {
+      GitHub: "https://github.com/jackyzha0/quartz",
+      "Discord Community": "https://discord.gg/cRFFHYye7t",
+    },
+  }),
 }
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
 	Component.ArticleTitle(),
-	Component.TagList(),
+	Component.TagList()
   ],
   left: [
     Component.PageTitle(),
     Component.Search(),
     Component.Darkmode(),
-	Component.Explorer(),
   ],
   right: [
 	Component.TableOfContents(),
