33547df6e70c9fc4b24ac0f5f22e9c8721f258e3
DCSG <zomblovr@hotmail.com>
2025-07-20 10:53:40 -0700

    .

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -15,15 +15,19 @@ export const sharedPageComponents: SharedLayout = {
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [Component.ArticleTitle(), Component.ContentMeta(), Component.TagList()],
+  beforeBody: [
+	Component.ArticleTitle(),
+	Component.TagList()
+  ],
   left: [
     Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.TableOfContents()),
   ],
-  right: [Component.Graph(), Component.Backlinks()],
+  right: [
+	Component.TableOfContents(),
+	Component.Backlinks(),
+  ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
