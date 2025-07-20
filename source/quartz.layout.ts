1e89c642c637299d137a33f2d680f65ac974e4b0
DCSG <zomblovr@hotmail.com>
2025-07-20 10:56:44 -0700

    .

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -5,24 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
-  }),
+  afterBody: [Component.Breadcrumbs(),],
+  footer: Component.Footer(),
 }
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
 	Component.ArticleTitle(),
-	Component.TagList()
+	Component.TagList(),
   ],
   left: [
     Component.PageTitle(),
     Component.Search(),
     Component.Darkmode(),
+	Component.Explorer(),
   ],
   right: [
 	Component.TableOfContents(),
